52ca47e39e62fb4501308b0b0d8974f5e7c28e61
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2026-01-15 11:51:53 +0000

    Export Excel

--- src/Controller/Admin/ActiviteCrudController.php
+++ src/Controller/Admin/ActiviteCrudController.php
@@ -31,10 +31,14 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\DependencyInjection\Attribute\AutowireLocator;
 use Symfony\Component\DependencyInjection\ServiceLocator;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 use Symfony\Component\Workflow\WorkflowInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Config\KeyValueStore;
 
@@ -149,6 +153,13 @@ class ActiviteCrudController extends AbstractCrudController
 
     public function configureActions(Actions $actions): Actions
     {
+        // Création e l'action Export Excel
+        $exportExcel = Action::new('exportExcel', 'Export Excel', 'fa fa-file-excel')
+            ->linkToCrudAction('exportExcel')
+            ->createAsGlobalAction()
+            ->setCssClass('btn btn-success')
+        ;
+
         $activiteWorkflow = $this->workflows->get('activite_workflow');
         $soumettre = Action::new('soumettre', "Soumettre")
             ->linkToCrudAction('soumettreActivite')
@@ -179,6 +190,7 @@ class ActiviteCrudController extends AbstractCrudController
 
         return $actions
             ->add(Crud::PAGE_INDEX, Action::DETAIL)
+            ->add(Crud::PAGE_INDEX, $exportExcel)
             ->add(Crud::PAGE_INDEX, $soumettre)
             ->add(Crud::PAGE_INDEX, $approuver)
             ->add(Crud::PAGE_INDEX, $valider)
@@ -437,5 +449,69 @@ class ActiviteCrudController extends AbstractCrudController
         $this->addFlash('success', "Activité rejetée avec succès!");
         return $this->redirect($context->getRequest()->headers->get('referer'));
     }
+
+    public function exportExcel(AdminContext $context): StreamedResponse
+    {
+        // Récupération directe via le repository
+//        $entityManager = $this->entity;
+        $repository = $this->entityManager->getRepository(Activite::class);
+
+        // Créer le QueryBuilder manuellement avec les mêmes filtres
+        $queryBuilder = $repository->createQueryBuilder('activite')
+//            ->addSelect('instance')
+            ->join('activite.instance', 'i')
+            ->andWhere('activite.statut <> :statut')
+            ->andWhere('i.nom = :nomInstance')
+            ->setParameter('nomInstance', 'Abidjan')
+            ->setParameter('statut', 'validee')
+            ->orderBy('activite.dateDebut', 'ASC')
+            ->addOrderBy('activite.dateFin', 'ASC');
+
+        $activites = $queryBuilder->getQuery()->getResult();
+
+        // 4. Génération du tableur
+        $spreadsheet = new Spreadsheet();
+        $sheet = $spreadsheet->getActiveSheet();
+
+        // Définition des colonnes (Tous les champs de l'entité Activite)
+        $headers = [
+            'A1' => 'Référence', 'B1' => 'Dénomination', 'C1' => 'Lieu',
+            'D1' => 'Date Début', 'E1' => 'Date Fin', 'F1' => 'Objectif',
+            'G1' => 'Contenu', 'H1' => 'Cibles', 'I1' => 'Responsable',
+            'J1' => 'Statut', 'K1' => 'Instance', 'L1' => 'Auteur'
+        ];
+
+        foreach ($headers as $cell => $text) {
+            $sheet->setCellValue($cell, $text);
+            $sheet->getStyle($cell)->getFont()->setBold(true);
+        }
+
+        $row = 2;
+        foreach ($activites as $activite) {
+            $sheet->setCellValue('A' . $row, $activite->getReference());
+            $sheet->setCellValue('B' . $row, $activite->getDenomination());
+            $sheet->setCellValue('C' . $row, $activite->getLieu());
+            $sheet->setCellValue('D' . $row, $activite->getDateDebut()?->format('d/m/Y'));
+            $sheet->setCellValue('E' . $row, $activite->getDateFin()?->format('d/m/Y'));
+            $sheet->setCellValue('F' . $row, strip_tags($activite->getObjectif()));
+            $sheet->setCellValue('G' . $row, strip_tags($activite->getContenu()));
+            $sheet->setCellValue('H' . $row, is_array($activite->getCible()) ? implode(', ', $activite->getCible()) : '');
+            $sheet->setCellValue('I' . $row, $activite->getResponsable());
+            $sheet->setCellValue('J' . $row, $activite->getStatut());
+            $sheet->setCellValue('K' . $row, (string) $activite->getInstance());
+            $sheet->setCellValue('L' . $row, $activite->getAuteur() ? $activite->getAuteur()->getNom().' '.$activite->getAuteur()->getPrenom() : '');
+            $row++;
+        }
+
+        $writer = new Xlsx($spreadsheet);
+
+        return new StreamedResponse(function() use ($writer) {
+            $writer->save('php://output');
+        }, 200, [
+            'Content-Type' => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+            'Content-Disposition' => ResponseHeaderBag::DISPOSITION_ATTACHMENT . '; filename="activites_validees_2025-2026.xlsx"',
+        ]);
+    }
+
 
 }
